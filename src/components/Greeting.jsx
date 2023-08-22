80d7a3bd56263a773bb9a7d285e352062eb94b35
Benson <njugunab655@gmail.com>
2023-08-22 17:59:06 +0300

    Display random start message

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchMessage } from '../redux/messageReducer';
 
 function Greeting() {
   const dispatch = useDispatch();
   const message = useSelector((state) => state.message);
+
+  useEffect(() => {
+    dispatch(fetchMessage());
+  }, []);
 
   return (
     <div>
